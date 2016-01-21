4f3a1cb5f74470b498e00aaedf5dd49fdf2b0dc8
Nanda <fernandaalvesmonteiro7@gmail.com>
2016-01-21 18:10:25 -0200

    adicionando servlets

---
+++ SistemaX/src/main/java/Servlets/ListarUsuario.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import com.mycompany.gerenciadores.GerenciadorDeUsuario;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author NandaPC
+ */
+@WebServlet(name = "ListarUsuario", urlPatterns = {"/ListarUsuario"})
+public class ListarUsuario extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+        doPost(request, response);
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        
+        try {
+            GerenciadorDeUsuario gerenciadorDeUsuario = new GerenciadorDeUsuario();
+            gerenciadorDeUsuario.listar();
+                        
+        } catch (Exception e) {
+        }
+    }
+}
